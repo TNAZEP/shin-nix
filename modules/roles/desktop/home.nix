67308f7735ad19e3fd8342f8886f766c6062db87
Jacob Ennaimi <jacob@ennaimi.com>
2025-11-21 09:38:01 +0900

    Initial merger of old arch hyprland config "HyprCog"

--- modules/roles/desktop/home.nix
+++ modules/roles/desktop/home.nix
@@ -1,5 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  # Desktop specific home-manager config (e.g. theming, hyprland config) can go here
+  imports = [
+    ./hyprland
+  ];
 }
---
+++ modules/roles/desktop/hyprland/default.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    
+    # We'll rely on the config file for now, but we can move settings here later
+    extraConfig = builtins.readFile ./config/hypr/hyprland.conf;
+  };
+
+  # Source other config files
+  xdg.configFile."hypr/hyprland".source = ./config/hypr/hyprland;
+  xdg.configFile."hypr/custom".source = ./config/hypr/custom;
+  
+  # Waybar
+  programs.waybar = {
+    enable = true;
+    style = ./config/waybar/style.css;
+  };
+  xdg.configFile."waybar/config".source = ./config/waybar/config;
+
+  # Rofi
+  home.packages = [ pkgs.rofi ];
+  xdg.configFile."rofi/config.rasi".source = ./config/rofi/config.rasi;
+
+  # Dunst
+  services.dunst = {
+    enable = true;
+    configFile = ./config/dunst/dunstrc;
+  };
+}
