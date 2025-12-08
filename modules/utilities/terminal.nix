d1da3a8e969b234fdec3f8b7c8747738c7547b76
TNAZEP <jacob@ennaimi.com>
2025-12-08 11:43:09 +0900

    update zsh config

--- modules/utilities/terminal.nix
+++ modules/utilities/terminal.nix
@@ -6,19 +6,39 @@
     };
 
   flake.homeModules.terminal =
-    { pkgs, ... }:
+    { config, lib, pkgs, ... }:
     {
-      programs.alacritty.enable = true;
+      programs.alacritty = {
+        enable = true;
+        settings = {
+          font.normal.family = "JetBrainsMono Nerd Font";
+        };
+        theme = "kanagawa_dragon";
+      };
+
+
 
       programs.zsh = {
         enable = true;
+
+        initExtra = ''
+          source ${./p10k.zsh}
+        '';
+
         oh-my-zsh = {
           enable = true;
           plugins = [
             "git"
           ];
-          theme = "robbyrussell";
         };
+
+        plugins = [
+          {
+            name = "powerlevel10k";
+            src = pkgs.zsh-powerlevel10k;
+            file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+          }
+        ];
       };
     };
 }
