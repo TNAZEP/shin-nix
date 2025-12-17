5e9c6f8f7ae845c0f9e8713345ac38842f4082c0
TNAZEP <jacob@ennaimi.com>
2025-12-17 18:10:33 +0900

    add lestallum placeholder hardware config

---
+++ modules/hosts/lestallum/_hardware-configuration.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  # fileSystems are managed by disko
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
