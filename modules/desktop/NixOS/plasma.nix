c7a4b1ee913c45a46403eb86b99fe330ee1dc8af
TNAZEP <jacob@ennaimi.com>
2025-12-18 21:48:04 +0900

    remove unused old modules

--- modules/desktop/NixOS/plasma.nix
+++
@@ -1,4 +0,0 @@
-# KDE/Plasma modules are split into:
-# - kde-core.nix       (Plasma6 desktop, system tools, theming, libraries)
-# - kde-multimedia.nix (Media apps: elisa, kdenlive, gwenview, etc.)
-{ }
--- modules/gaming/default.nix
+++
@@ -1,5 +0,0 @@
-# Gaming modules are split into:
-# - steam.nix     (Steam, gamemode, mangohud, protontricks)
-# - minecraft.nix (Prism Launcher, Modrinth)
-# - retro.nix     (Classic game ports like Shipwright)
-{ }
